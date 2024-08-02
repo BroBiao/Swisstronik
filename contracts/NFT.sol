7e0a8f9f073a37c5dbfe70f60cc947524b566f37
BroBiao <brobiao29@gmail.com>
2024-08-02 22:48:21 +0800

    first commit

---
+++ contracts/NFT.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+contract NFT_OCEAN is ERC721, ERC721Enumerable, Ownable {
+    string private _baseURIextended;
+    //Set mint limit
+    uint256 public constant MAX_SUPPLY = 500;
+
+    constructor() ERC721("nft_ocean", "NFT_OCEAN") {
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 firstTokenId, uint256 batchSize) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, firstTokenId, batchSize);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function setBaseURI(string memory baseURI_) external onlyOwner() {
+        _baseURIextended = baseURI_;
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseURIextended;
+    }
+
+    function mint(uint numberOfTokens) public payable {
+        uint256 ts = totalSupply();
+        require(ts + numberOfTokens <= MAX_SUPPLY, "Purchase would exceed max tokens");
+
+        for (uint256 i = 0; i < numberOfTokens; i++) {
+            _safeMint(msg.sender, ts + i);
+        }
+    }
+
+    function withdraw() public onlyOwner {
+        uint balance = address(this).balance;
+        payable(msg.sender).transfer(balance);
+    }
+}
